2f98cc8fd2555d50035ecf7d9a3ce4a48b45730c
Hashim Hossam <104256650+Hhmmmw@users.noreply.github.com>
2022-06-04 11:57:56 +0200

    init1

---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+import { defineNuxtConfig } from 'nuxt'
+
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+    typescript: {
+        shim: false
+    }
+    //,
+    // modules: [
+    //     '@nuxtjs/axios',
+    // ],
+    // axios: {
+    //     proxy: true
+    // }
+})
---
+++ pages/airports/_code.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container mx-auto my-5">
+    <h1 class="text-2xl leading-7 font-semibold">This is the Airport Detail Page</h1>
+    <p>This page contains the specific information about the <strong>{{ airport.abbreviation }}</strong> airport.</p>
+  </div>
+</template>
+
+<script>
+import airports from '~/data/airports.js'
+
+export default {
+  name: 'AirportDetailPage',
+  asyncData ({ route }) {
+    const airport = airports.filter(airport => airport.abbreviation === route.params.code.toUpperCase())[0]
+
+    return {
+      airport
+    }
+  }
+}
+</script>
